6ceaefed26c94ff31ca09d3f31455fc23a4c7238
moriya <amamori6+s@gmail.com>
2017-10-30 18:14:31 +0900

    更新

--- app/src/main/java/com/example/sample/pokemon/MainActivity.java
+++ app/src/main/java/com/example/sample/pokemon/MainActivity.java
@@ -7,6 +7,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     //定数を定義
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     //値を更新する
     //更新した値をText化して表示する
+
     //起動後の処理
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         invalidationProcessingOfButtonByQuantity();
         convertToString();
     }
+
     //Viewを初期化
     private void unionUi() {
         setContentView(R.layout.activity_main);
@@ -45,10 +51,36 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickByMinusButton(View view) {
         quantity--;
-        convertToString();
         invalidationProcessingOfButtonByQuantity();
+        convertToString();
     }
-    //数量値で非活性処理
+    //現在時刻表示
+    // ミリ秒
+
+private void nuu
+System.currentTimeMillis();
+
+    // Date型にする場合
+    Date now = new Date(System.currentTimeMillis);
+
+
+
+    // 日時のフォーマットオブジェクト作成」
+    public static String getNowDate(){
+        // 現在日時の取得
+        Date now = new Date(System.currentTimeMillis());
+        DateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日 HH時mm分");
+
+        // フォーマット
+        String nowText = formatter.format(now);
+
+        // 表示
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText(nowText);
+    }
+
+
+    //限界値でトーストも表示
     private void invalidationProcessingOfButtonByQuantity() {
         if (quantity <= QUANTITY_MIN) {
             quantity = QUANTITY_MIN;
